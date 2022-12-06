8cee4608d31dc7b56e2a28d445d22e2093621bf5
romaing <rocambille@gmail.com>
2022-12-06 10:05:01 +0100

    fetched articles from API

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import logo from "../assets/logo.svg";
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${import.meta.env.VITE_BACKEND_URL ?? "http://localhost/5000"}/articles`
+    )
+      .then((response) => response.json())
+      .then((data) => setArticles(data));
+  }, []);
+
+  console.warn(articles);
+
   return (
     <>
       <header className="App-header">
