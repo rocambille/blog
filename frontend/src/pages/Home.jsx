2d5cb9dbdfa1f7a7818fa1dc7113a7a7b67c3c6d
romaing <rocambille@gmail.com>
2022-12-06 09:32:51 +0100

    refactored Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,23 @@
-import Counter from "../components/Counter";
 import logo from "../assets/logo.svg";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Blog !</h1>
+      </header>
+      <main>
+        <article>
+          <h1>Lorem ipsum 1</h1>
+          <p>Lorem ipsum 1</p>
+        </article>
+        <article>
+          <h1>Lorem ipsum 2</h1>
+          <p>Lorem ipsum 2</p>
+        </article>
+      </main>
+      <footer>footer</footer>
+    </>
   );
 }
